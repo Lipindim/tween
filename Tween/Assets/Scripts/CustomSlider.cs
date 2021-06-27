35a4be7b652f01c03f9dc6e342866029c7839b6f
lipindim <lipindim@gmail.com>
2021-06-27 11:17:05 +1000

    Домашняя работа 7

---
+++ Tween/Assets/Scripts/CustomSlider.cs
@@ -0,0 +1,46 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CustomSlider : Slider
+{
+    public static string ChangeButtonType => nameof(_animationType);
+    public static string CurveEase => nameof(_curveEase);
+    public static string Duration => nameof(_duration);
+
+    [SerializeField]
+    private AnimationType _animationType = AnimationType.Appearance;
+
+    [SerializeField]
+    private Ease _curveEase = Ease.Linear;
+
+    [SerializeField]
+    private float _duration = 2.0f;
+
+    private RectTransform _rectTransform;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        _rectTransform = GetComponent<RectTransform>();
+    }
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        ActivateAnimation();
+    }
+
+    private void ActivateAnimation()
+    {
+        switch (_animationType)
+        {
+            case AnimationType.Appearance:
+                _rectTransform.DOScale(0.0f, 0.0f);
+                _rectTransform.DOScale(1.0f, _duration).SetEase(_curveEase);
+                break;
+        }
+    }
+}
---
+++ Tween/Assets/Scripts/Editor/CustomSliderEditor.cs
@@ -0,0 +1,41 @@
+﻿using UnityEditor;
+using UnityEditor.UI;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+
+[CustomEditor(typeof(CustomSlider))]
+public class CustomSliderEditor : SliderEditor
+{
+
+    public override VisualElement CreateInspectorGUI()
+    {
+        var root = new VisualElement();
+
+        var changeButtonType = new PropertyField(serializedObject.FindProperty(CustomSlider.ChangeButtonType));
+        var curveEase = new PropertyField(serializedObject.FindProperty(CustomSlider.CurveEase));
+        var duration = new PropertyField(serializedObject.FindProperty(CustomSlider.Duration));
+
+        var tweenLabel = new Label("Settings Tween");
+        var intractableLabel = new Label("Intractable");
+
+        root.Add(tweenLabel);
+        root.Add(changeButtonType);
+        root.Add(curveEase);
+        root.Add(duration);
+
+        root.Add(intractableLabel);
+        root.Add(new IMGUIContainer(OnInspectorGUI));
+
+        return root;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUI.BeginChangeCheck();
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
